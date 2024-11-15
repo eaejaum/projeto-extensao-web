27d4caf4767805a4baa7e1485894f3b636b796a7
eaejaum <jpedroalmeida179@gmail.com>
2024-11-14 21:08:20 -0300

    Removendo informações desnecessarias

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,16 +4,7 @@ import './Footer.css'
 const Footer = () => {
     return (
         <div className="principal bg-body-tertiary">
-            <div>
-                L & BY Pratas
-            </div>
-            <div>
-                <span>Copyright L & BY Pratas - 2024. Todos os direitos reservados.</span>
-            </div>
-            <div className="redes-sociais">
-                <button type="button" class="btn btn-dark">Instagram</button>
-                <button type="button" class="btn btn-dark">Whatsapp</button>
-            </div>
+            <span>Copyright L & BY Pratas - 2024. Todos os direitos reservados.</span>
         </div>
     );
 };
