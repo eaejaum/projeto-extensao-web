bc5946cf12683fe6138641d7d4f2fba338e4ef73
eaejaum <jpedroalmeida179@gmail.com>
2024-11-18 11:16:17 -0300

    removendo botao instagram do contato

--- client/src/Routes/Contact.jsx
+++ client/src/Routes/Contact.jsx
@@ -93,29 +93,7 @@ const Contact = () => {
         <hr />
         <p>lbypratas@gmail.com</p>
       </div>
-      <div
-        style={{
-          padding: "2rem 0",
-          background: "#E4E0DA",
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        <div
-          style={{
-            border: "3px solid #BFA78A",
-            borderRadius: "100%",
-            width: "4.5rem",
-            height: "4.5rem",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            cursor: "pointer",
-          }}
-        >
-          <i class="bi bi-instagram fs-1" style={{ color: "#BFA78A" }}></i>
-        </div>
-      </div>
+      
     </>
   );
 };
