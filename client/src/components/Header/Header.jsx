30a98e9c9b833e46429380f171d7a72d3fe3dfe4
eaejaum <jpedroalmeida179@gmail.com>
2024-11-17 21:48:24 -0300

    adicionando Link para navegação e corrigindo estilização

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,16 +1,16 @@
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
         <nav className="navbar bg-body-tertiary">
-            <div className="principal container-fluid">
-                <span className="navbar-brand mb-0 h1">LOGO</span>
+            <div className="container-fluid" style={{ padding: '.6rem' }}>
+                <span className="navbar-brand mb-0 h1">L&BY Pratas</span>
                 <div className="navegacao">
-                    <a href="#">Início</a>
-                    <a href="#">Produto</a>
-                    <a href="#">Contato</a>
-                    <a href="#">Carrinho</a>
-                    <a href="#">Newsletter</a>
+                    <Link to="/">Início</Link>
+                    <Link to="/contact">Contato</Link>
+                    <Link to="/shoppingcart">Carrinho</Link>
+                    <Link to="/newsletter">Newsletter</Link>
                 </div>
             </div>
         </nav>
