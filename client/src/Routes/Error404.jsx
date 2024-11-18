ade4104a2ee6a9673de96b779f281bd78ac75332
eaejaum <jpedroalmeida179@gmail.com>
2024-11-17 21:10:15 -0300

    pagina nao encontrada

---
+++ client/src/Routes/Error404.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Error404 = () => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>Página não encontrada!</h1>
+      <p>Sentimos muito, mas a página que você deseja acessar, não foi encontrada :(</p>
+    </div>
+  )
+}
+
+export default Error404
