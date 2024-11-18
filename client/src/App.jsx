0e821862830436bd36e25991a91a7f7b164b9276
eaejaum <jpedroalmeida179@gmail.com>
2024-11-17 22:18:10 -0300

    excluindo div

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,11 @@ import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
+    <>
       <Header />
       <Outlet />
       <Footer />
-    </div>
+    </>
   );
 }
 
