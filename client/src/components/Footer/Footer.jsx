5966e30b4eed2d8d8c603481b032d8796ec091e7
eaejaum <jpedroalmeida179@gmail.com>
2024-11-18 11:15:57 -0300

    adicionando botão do instagram no footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div 
-            style={{ 
-                display: 'flex', 
-                justifyContent: 'center', 
-                alignItems: 'center',
-                padding: '.7rem', 
-                backgroundColor: '#A2907A',
-            }}
+  return (
+    <>
+      <div
+        style={{
+          padding: "2rem 0",
+          background: "#E4E0DA",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            border: "3px solid #BFA78A",
+            borderRadius: "100%",
+            width: "4.5rem",
+            height: "4.5rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            cursor: "pointer",
+          }}
         >
-            <span style={{ color: "#DDDAD6" }}>Copyright L & BY Pratas - 2024. Todos os direitos reservados.</span>
+          <i class="bi bi-instagram fs-1" style={{ color: "#BFA78A" }}></i>
         </div>
-    );
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: ".7rem",
+          backgroundColor: "#A2907A",
+        }}
+      >
+        <span style={{ color: "#DDDAD6" }}>
+          Copyright L & BY Pratas - 2024. Todos os direitos reservados.
+        </span>
+      </div>
+    </>
+  );
 };
 
-export default Footer
+export default Footer;
