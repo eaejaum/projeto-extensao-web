f5f0dca4cf76524a8516ed0befd46fd9de500159
Gabriel Queiroz <142359384+QsGabriel@users.noreply.github.com>
2024-11-20 16:50:56 -0300

    cors é horrivel

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mysql = require('mysql2');
+const cors = require('cors');
 
 const db = mysql.createPool({
     host:"localhost",
@@ -9,11 +10,30 @@ const db = mysql.createPool({
     database:"lbypratas",
 })
 
+const corsOptions = {
+    origin: 'http://localhost:3000/',   // Permitir apenas a origem desejada
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'], // Cabeçalhos permitidos
+    credentials: true,  // Permitir o envio de cookies e autenticação
+  };
+  
+  app.use(cors());
+
 app.get("/",(req, res)=>{
     let SQL = "SELECT * FROM usuario";
     db.query(SQL,(err,result)=>{
         console.log(err,result);
     });
 })
+
+app.get('/estoque', (req, res) => {
+    const query = 'SELECT * FROM ITEM'; // Consulta para pegar todos os itens
+    db.query(query, (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: err.message }); // Retorna erro, se houver
+      }
+      res.json(results); // Retorna os dados da tabela ITEM como JSON
+    });
+  });
 
 app.listen(3001);
