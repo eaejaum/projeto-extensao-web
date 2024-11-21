ef0c84a8992b56ebedf0edfd37a6d3a8acab6b79
eaejaum <jpedroalmeida179@gmail.com>
2024-11-20 23:48:18 -0300

    rolagem para div dos produtos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
@@ -5,10 +6,23 @@ import { Outlet } from "react-router-dom";
 
 
 const App = () => {
+  const productsRef = useRef(null);
+
+  const scrollToProducts = () => {
+    if (productsRef.current) {
+      productsRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
+
   return (
     <>
-      <Header />
-      <Outlet />
+      <Header scrollToProducts={scrollToProducts}/>
+      <div ref={productsRef} style={{ marginTop: "2rem" }}>
+        <Outlet />
+      </div>
       <Footer />
     </>
   );
